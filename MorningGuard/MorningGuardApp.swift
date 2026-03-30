820687b515c864e068a99996fffbcbb9dda21960
MorningGuard <hello@morningguard.com>
2026-03-30 15:11:32 -0700

    Initial Commit

---
+++ MorningGuard/MorningGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningGuardApp.swift
+//  MorningGuard
+//
+//  Created by Alex Liu on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct MorningGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorningGuardTests/MorningGuardTests.swift
@@ -0,0 +1,16 @@
+//
+//  MorningGuardTests.swift
+//  MorningGuardTests
+//
+//  Created by Alex Liu on 2026-03-30.
+//
+
+import Testing
+
+struct MorningGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
